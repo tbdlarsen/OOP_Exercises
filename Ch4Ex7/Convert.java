41c146e918b5048fa97b1e3360474d7993b2ca02
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-09-21 19:43:52 +0200

    added comments

--- Ch4Ex7/Convert.java
+++ Ch4Ex7/Convert.java
@@ -1,14 +1,17 @@
 public class Convert{
+    //instance variables
     int big;
     byte small;
     
-    
+    //constructor
     Convert(int a, byte b){
     big = a;
     small = b;
 
     }
 
+
+    //method
     void update(){
          small = (byte) big;
 
--- Ch4Ex7/Main.java
+++ Ch4Ex7/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        //instance variables
         int first = 90;
         byte last = 10;
 
-
+        //create new object
         Convert nums1 = new Convert(first,last);
         
-
+        
         nums1.update();
 
     }
