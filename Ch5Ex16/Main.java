a6360a1f8f977d6d7f7930072a5018fb172c586d
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-09-23 13:50:12 +0200

    formatted code

--- Ch5Ex16/Main.java
+++ Ch5Ex16/Main.java
@@ -1,21 +1,14 @@
-public class Main{
+
+public class Main {
 
     public static void main(String[] args) {
         int x = 19;
-      
 
-
-        String msg = (x & 1) == 1 ? "odd":"even";
+        String msg = (x & 1) == 1 ? "odd" : "even";
 
         //String msg = x % 1 == 1 ? "odd":"even";
-        
-   
         System.out.println("x is " + msg);
 
-
-
     }
-
-
 
 }
