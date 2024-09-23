026c31932e3800a3d7de2ed2ccaa09bf266f1d41
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-09-23 11:13:58 +0200

    fisished exercise added answer as comment, typecast fail

---
+++ Ch4Ex11/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+   
+    
+    boolean done = true;
+    // bool cannot be converted to int, unlike C language. 
+    int x = (int) done;
+
+    }
+
+}
