ea231544d6c6a1491604a205f1140429b586f803
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-09-23 11:30:47 +0200

    finished task

---
+++ Ch4Ex12/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        int x = 1;
+        int y =0;
+
+        boolean a = true;
+        boolean b = false;
+
+        
+        x = a;
+        y= b;
+
+        the integers and booleans are not the same
+        */
+       
+    
+
+    }
+
+}
