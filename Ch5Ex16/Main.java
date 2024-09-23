0242ca1acdfcf190b42058aee234a38bca697783
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-09-23 12:05:36 +0200

    started task, NOT done, use bitwise operator instead of mod

---
+++ Ch5Ex16/Main.java
@@ -0,0 +1,24 @@
+public class Main{
+
+    public static void main(String[] args) {
+        int x = 19;
+      
+       
+
+
+        
+
+        String msg = (x % 1 == 0) ? "odd":"even";
+
+        // x, &,==, ?, :, "odd", and "even".
+        
+   
+        System.out.println("x is " + msg);
+
+
+
+    }
+
+
+
+}
