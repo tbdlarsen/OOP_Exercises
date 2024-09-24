534ccf684d1200f27607ee3a1ff9cc155bca4a11
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-09-24 22:41:31 +0200

    added the setter and getter methods to Point2D class.

---
+++ Ch8Ex5/Point2D.java
@@ -0,0 +1,24 @@
+public class Point2D {
+
+    //instance variables
+    private int x;
+    private int y;
+
+    //setter and getter methods
+    void setX(int x){
+        this.x = x;
+    }
+
+    void setY(int y){
+        this.y = y;
+    }
+
+    int getx(){
+        return this.x;
+    }
+
+    int getY(){
+        return this.y;
+    }
+
+}
