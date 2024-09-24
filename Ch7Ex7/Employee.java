f189143732e58e2e307d516cd47e6f0589e65159
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-09-24 22:53:13 +0200

    added comment to employee class

--- Ch7Ex7/Employee.java
+++ Ch7Ex7/Employee.java
@@ -1,12 +1,13 @@
 public class Employee {
-String name;
-boolean retired;
-double salary;
-public static void main(String[] args) {
-        Employee emp = new Employee();
-        System.out.println(emp.name);
-        System.out.println(emp.retired);
-        System.out.println(emp.salary);
+    String name;
+    boolean retired;
+    double salary;
+    
+    public static void main(String[] args) {
+            Employee emp = new Employee();
+            System.out.println(emp.name);
+            System.out.println(emp.retired);
+            System.out.println(emp.salary);
     }
 
 
