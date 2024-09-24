4035f717f1b89b279b3609d50b1e647d6937d3bd
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-09-24 23:21:35 +0200

    added comments to main class

--- Ch8ex5-6-7/Main.java
+++ Ch8ex5-6-7/Main.java
@@ -1,16 +1,21 @@
 public class Main{
 
     public static void main(String[] args) {
+
+        //create and set values for two random points
         Point2D point0 = new Point2D();
         
         point0.setX(2);
         point0.setY(5);
-        System.out.println("value of x: " + point0.getX() + " value of y: " + point0.getY());
 
         Point2D point1 = new Point2D();
         point1.setX(1);
         point1.setY(2);
-        
+
+
+
+        //print test to see if methods work correctly
+        System.out.println("value of x: " + point0.getX() + " value of y: " + point0.getY());
         System.out.println("distance between point 0 and 1: " + point1.distance(point0));
 
         
