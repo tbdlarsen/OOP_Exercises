2d73a78ad11cd6156897b196a68d272fd4464965
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-10-07 08:34:03 +0200

    finished Ch5Ex9

---
+++ week4/Ch5Ex9/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public int x = 10;
+    //boolean yes = (x = 20);
+
+
+
+}
+//the code does not compile since it is trying to convert int to bool value, use doube == to compare. 
