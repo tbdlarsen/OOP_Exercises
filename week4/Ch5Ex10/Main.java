0d4485aa87f22044905547577167233ce9351d27
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-10-07 08:41:24 +0200

    finished Ch5Ex10

---
+++ week4/Ch5Ex10/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    
+    public int x = 23;
+    boolean yes = (x == 20);
+    
+    
+    public static void main(String[] args) {
+        Main obj = new Main();
+
+       System.out.println(obj.yes);
+       
+    }
+
+
+}
+//program compiles because there are two == which compares the values
