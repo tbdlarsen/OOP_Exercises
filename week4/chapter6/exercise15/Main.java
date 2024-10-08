be81d0acf57bd929f38082bf731a4770ece007d9
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-10-08 20:16:43 +0200

    finished chapter6 exercise 15, for loop training

---
+++ week4/chapter6/exercise15/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+
+    
+
+    public void printer(){
+        for(byte b = 10; b >= 1; b--){
+            System.out.println(b);
+
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.printer();
+    }
+
+}
