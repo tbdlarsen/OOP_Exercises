2a17269638df35ba911b1d9885c5016f1d8cd4b4
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-10-08 20:37:46 +0200

    finished chapter6 exercise18, for loop training

--- week4/chapter6/exercise18/Main.java
+++ week4/chapter6/exercise18/Main.java
@@ -3,12 +3,14 @@ public class Main{
     public void pyramidPrinter(){
         int layers = 4;
 
-        for (int i = 0; i < layers; i++) {
+        for (int i = 1; i <= layers; i++) {
             
             for (int j = i; j < layers; j++) {
-                System.out.print("a");
+                System.out.print(" ");
             }
-            
+            for (int k = 1; k <= (2*i-1); k++) {
+                System.out.print("*");
+            }
 
             System.out.println(" ");
             
