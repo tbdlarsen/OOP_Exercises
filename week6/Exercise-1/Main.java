77ea1d1a736e43f1107b6f9b50af0304406ba375
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-10-10 09:20:39 +0200

    added exercies md and finised exercise 1

---
+++ week6/Exercise-1/Main.java
@@ -0,0 +1,24 @@
+/*
+public class Main {
+
+    int y;
+    int n;
+    //int e;
+    
+    public void round() {
+        try {
+            int x = 100 / y;
+            int[] es = new int[n];
+           // e = es[Math.round(x)];
+        //} catch (ArithmeticException e1) {
+            //...
+        } catch (NegativeArraySizeException e2) {
+            //...
+        } catch (java.util.NoSuchElementException e3) {
+            //...
+        }
+
+    }
+    // i would use multicatch or option a, because it is clearer what exeption is catched
+
+}
