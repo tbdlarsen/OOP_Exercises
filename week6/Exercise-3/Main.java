e5e7053c6e138f836f143fb4f55399eae17fdad9
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-10-10 09:34:49 +0200

    finished exercise 3

---
+++ week6/Exercise-3/Main.java
@@ -0,0 +1,26 @@
+public class Main{
+
+    /*
+    void errorFunc(){
+        try {
+            // The following statement throws NumberFormatException
+        int luckNumber = Integer.parseInt("Hello");
+        } catch (Exception e) {
+        // Handle the exception here
+        } catch (NumberFormatException e) {
+        // Handle the exception here
+        }
+    }
+    */
+
+
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        
+        //obj.errorFunc();
+
+
+    }
+}
+//code does not compile because NumberFormatExption is child class of Exeption 
