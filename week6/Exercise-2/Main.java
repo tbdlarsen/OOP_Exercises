410b137a4096ccd56c274e076b4d4303e1b4fd39
thorbjørn dige larsen <tbdlarsen@gmail.com>
2024-10-10 09:28:02 +0200

    finished exercise 2

---
+++ week6/Exercise-2/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    public void rounder() {
+        try {
+            int x = 100 / 0;
+        } catch (ArithmeticException e) {
+            String errorMessage = e.getLocalizedMessage();
+            System.out.println(errorMessage);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.rounder();
+
+    }
+
+}
